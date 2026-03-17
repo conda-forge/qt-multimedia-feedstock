4d3c5a1d281a8f80c767c529fe353f07189d4331
Julien Schueller <schueller@phimeca.com>
2026-03-17 14:16:44 +0100

    v1

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,20 +1,15 @@
 @echo on
 
-mkdir build
-cd build
-
 cmake -LAH -G "Ninja" ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
     -DCMAKE_UNITY_BUILD=ON -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 ^
     -DCMAKE_MESSAGE_LOG_LEVEL=STATUS ^
-    -DQT_NO_MSVC_MIN_VERSION_CHECK=ON ^
-    -DQT_DEFAULT_MEDIA_BACKEND=ffmpeg ^
-    ..
+    -B build .
 if %ERRORLEVEL% neq 0 exit 1
 
-cmake --build . --target install --config Release
+cmake --build build --target install --config Release
 if %ERRORLEVEL% neq 0 exit 1
 
 :: unversioned exes must avoid clobbering the qt5 packages, but versioned dlls still need to be in PATH
